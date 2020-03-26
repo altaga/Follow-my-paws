8cdfbb44e1d3cc8a31507b9063dc3409d16c9662
Víctor Altamirano <v.a.i@hotmail.com>
2020-03-26 03:10:14 -0600

    Update credentials.js
    Changing the @#$& credentials

--- WebPage/js/credentials.js
+++ WebPage/js/credentials.js
@@ -1,21 +1,4 @@
-var publics = "AKIAIZLBOFB3BJRVNACA";
-var privates = "Vn5DXKbCr8kwSchzSONNLNjO54oi+R0AwCwwVl9";
-var endaws = 'dynamodb.ap-northeast-1.amazonaws.com';
+var publics = "xxxxxxxxxxxxxxxxxxxxxxxx";
+var privates = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+var endaws = 'xxxxxxxxxx.ap-northeast-1.amazonaws.com';
 var regaws =  "ap-northeast-1";
-
-// Setup the MQTT!!!
-
-var ibmu="a-lhnt68-7vlby0itgu"
-var ibmp= "@D_i3qAIW(kFul@P)U"
-var url="lhnt68.messaging.internetofthings.ibmcloud.com";
-var port = 8883;
-var keyibm = "a:lhnt68:7vlby0itgu";
-var topic1 = "iot-2/type/ESP8266/id/nodemcu/evt/status/fmt/json";
-var topic2 = "iot-2/type/Raspberry/id/001/evt/+/fmt/+";
-var topic3 = "iot-2/type/Raspberry/id/002/evt/+/fmt/+";
-var topic4 = "iot-2/type/ESP8266/id/Semaforo/evt/status/fmt/json";
-var destiny = "iot-2/type/ESP8266/id/Semaforo/cmd/status/fmt/json";
-
-// Setup the hospital!!!
-
-var clinic = "Example Hospital 1"
